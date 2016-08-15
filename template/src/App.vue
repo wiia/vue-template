663b8bfe75f374ef3626c862416d44fc0809b17e
wiia <250569174@qq.com>
2016-08-15 20:39:36 +0800

    new project

---
+++ template/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="less">
+@import './assets/css/index.less';
+</style>
